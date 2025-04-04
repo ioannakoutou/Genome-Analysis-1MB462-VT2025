1f9cd998d5e9c6613bcd442a7753eb19c91d21ba
Ioanna Kyriaki Koutou <ioko9176@rackham3.uppmax.uu.se>
2025-04-04 15:56:36 +0200

    Rename Flye script to 04_flye.sh to match new analysis structure

--- code/04_flye.sh
+++ code/04_flye.sh
@@ -15,7 +15,7 @@ module load Flye/2.9.5
  
 #
 export IN="/home/ioko9176/genome_analysis/Genome-Analysis-1MB462-VT2025/data/dna_seq/nanopore/chr3_clean_nanopore.fq.gz"
-export OUT="/home/ioko9176/genome_analysis/Genome-Analysis-1MB462-VT2025/analyses/00_genome_assembly/flye_output"
+export OUT="/home/ioko9176/genome_analysis/Genome-Analysis-1MB462-VT2025/analyses/01_genome_assembly/flye_output"
 export THREADS="8"
 
 
