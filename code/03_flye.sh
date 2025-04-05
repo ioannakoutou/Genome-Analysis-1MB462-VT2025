d494894c9cd5c7e17b271c38a487ad190c8ba322
Ioanna Kyriaki Koutou <ioko9176@rackham3.uppmax.uu.se>
2025-04-05 11:24:35 +0200

    Minor edit to 03_flye.sh

--- code/03_flye.sh
+++ code/03_flye.sh
@@ -16,7 +16,7 @@ module load Flye/2.9.5
 #
 export IN="/home/ioko9176/genome_analysis/Genome-Analysis-1MB462-VT2025/data/dna_seq/nanopore/chr3_clean_nanopore.fq.gz"
 export OUT="/home/ioko9176/genome_analysis/Genome-Analysis-1MB462-VT2025/analyses/01_genome_assembly/flye_output"
-export THREADS="8"
+export THREADS=8
 
 
 # run flye
